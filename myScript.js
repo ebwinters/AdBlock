b7781b9f216152e5c15effc00eb366cead778153
ebwinters@comcast.net <ebwinters@comcast.net>
2019-05-11 19:51:27 -0400

    regex list starting

--- myScript.js
+++ myScript.js
@@ -6,3 +6,35 @@ btn.appendChild(t);
 //Appending to DOM 
 document.body.appendChild(btn);
 
+//Popular ad div IDs and classes
+/*
+	google_image_div
+	banner
+	ad_unit
+	ad-container
+	data-slot
+	google_image_div
+	contains "ad-wrapper"
+	beacon
+	hireme
+	contains "google_ads_iframe"
+	textwidget
+	fh
+*/
+
+//a tag classes nested in divs 
+/*
+	google_image_div
+
+*/
+
+//a tag props nested in divs
+/*
+	data-clc-click
+*/
+
+//iframe classes
+/*
+	ym-ad-choices
+
+*/
